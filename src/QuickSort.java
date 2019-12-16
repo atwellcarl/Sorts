56a165117b389b1d7e1b49e2f7c77b11393ebe1c
Carl Atwell <carl.j.atwell@gmail.com>
2019-12-16 13:17:12 -0600

    Quicksort + user input

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,39 @@
+import java.util.Random;
 
 public class QuickSort {
-
+	
+	public void sort(int[] arr, int left, int right){
+		//if the partition is greater than one
+		if(left < right) {
+		
+			int pivot = partition(arr, left, right);
+			
+			this.sort(arr, left, pivot - 1);
+			this.sort(arr, pivot + 1, right);
+		}
+	}
+	private int partition(int arr[], int left, int right) {
+		int pivotVal = arr[right];
+		int tracker = left -1;
+		//for all indexes in range of partiton
+		for(int index = left; index < right; index++) {
+			//if the value at this index is less than the pivot keep it the left 
+			if(arr[index] <= pivotVal) {
+				tracker++;
+				swap(arr, tracker, index);
+			}
+		}
+		
+		int temp = arr[tracker + 1];
+		arr[tracker + 1] = arr[right];
+		arr[right] = temp;
+		
+		return tracker + 1;
+	}
+	
+	private void swap(int arr[], int tracker, int index) {
+		int temp = arr[tracker];
+		arr[tracker] = arr[index];
+		arr[index] = temp;
+	}
 }
--- src/Sorters.java
+++ src/Sorters.java
@@ -1,18 +1,93 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Sorters {
 
 	public static void main(String[] args) {
 		Sorters sorters = new Sorters();
-		int[] arr = {2, 5, 1, 3, 5, 2, 4, 6, 3, 2 ,8, 6, 44, 3, 5, 6, 4567, 4657};
+		Scanner scan = new Scanner(System.in);
+		System.out.println("__________Sorting Algorithms__________");
+		System.out.println("BubbleSort(ID = 1) QuickSort(ID = 2)");
+		int id = -1;
 		
-		BubbleSort bubbleSort = new BubbleSort();
-		bubbleSort.sort(arr);
-		
-		sorters.print(arr);
-	}
-	private void print(int arr[]) {
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
+		while(id != 0) {
+			System.out.print("Enter an algorithm id: ");
+			id = scan.nextInt();
+			System.out.println();
+			
+			if(id == 0) {
+				break;
+			}
+			System.out.print("How many number to sort? ");
+			int length = scan.nextInt();
+			int[] arr = sorters.generate(length);
+			if(id == 1) {
+				System.out.println("Array sorted = " + sorters.isSorted(arr));
+				BubbleSort bubbleSort = new BubbleSort();
+				bubbleSort.sort(arr);
+				System.out.println("Array Sorted = " + sorters.isSorted(arr));
+				sorters.print(arr, scan);
+			}
+			else if(id == 2) {
+				System.out.println("Array sorted = " + sorters.isSorted(arr));
+				QuickSort quickSort = new QuickSort();
+				quickSort.sort(arr, 0, arr.length - 1);
+				System.out.println("Array Sorted = " + sorters.isSorted(arr));
+				sorters.print(arr, scan);
+			}
+			else{
+				System.out.println("Invalid input, please try again");
+			}	
+			
+			
 		}
+		
+		System.out.println("Adios!");
+		scan.close();
+		
+	}
+	
+	
+	private void print(int arr[], Scanner scan) {
+		
+		System.out.print("Would you like the array printed? (yes/no): ");
+		String print = scan.next();
+		
+		if(print.equals("yes")) {
+			for(int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+		else if(print.equals("no")) {
+			return;
+		}
+		else {
+			System.out.println("Invalid Input!");
+			this.print(arr, scan);
+		}
+	}
+	
+	private int[] generate(int length) {
+		int arr[] = new int[length];
+		Random ran  = new Random();
+		for(int i = 0; i< length; i++) {
+			arr[i] = ran.nextInt();
+		}
+		
+		return arr;		
+	}
+	
+	private boolean isSorted(int[] arr) {
+		boolean sorted = true;
+		
+		int prev = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] < prev) {
+				return false;
+			}
+			prev = arr[i];
+		}
+		return sorted;
 	}
 }
